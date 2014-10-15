c3a69bce95509e6b2565200a2df0bf10513dad05
Liang Ding <DL88250@gmail.com>
2014-10-15 15:26:59 +0800

    .

--- sql/install.sql
+++ sql/install.sql
@@ -32,11 +32,14 @@ CREATE TABLE `client` (
   `id` varchar(64) NOT NULL,
   `user_id` varchar(64) DEFAULT NULL,
   `type` varchar(45) DEFAULT NULL,
-  `device_id` varchar(64) DEFAULT NULL,
+  `device_id` varchar(64) DEFAULT NULL COMMENT '多个的话以 , 分割',
   `created` datetime DEFAULT NULL,
   `updated` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+
 
 delimiter $$
 
