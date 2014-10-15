ba26024742466bf349c3511e2406899c2565b58a
Liang Ding <DL88250@gmail.com>
2014-10-15 13:59:25 +0800

    #5

--- client/js/demo/static/msg-js.js
+++ client/js/demo/static/msg-js.js
@@ -2,207 +2,210 @@
  * gopush-cluster javascript sdk
  */
 
-(function(){
+(function () {
 
-	var getScript = function(options){
-		// JSONP
-		var callback = 'callback_' + Math.floor(new Date().getTime() * Math.random()).toString(36);
-		var head = document.getElementsByTagName("head")[0];
-		var script = document.createElement('script');
-		options = options || {};
-		GoPushCli[callback] = options.success || function(){};
-		script.type = 'text/javascript';
-		script.charset = 'UTF-8';
-		script.onload = script.onreadystatechange = function(_, isAbort){
-			if ( isAbort || !script.readyState || /loaded|complete/.test( script.readyState ) ) {
-				script.onload = script.onreadystatechange = null;
-				head.removeChild(script);
-				script = null;
-			}
-		};
-		//script.src = options.url + ((/\?/).test(options.url) ? '&' : '?') + 'callback=GoPushCli.' + callback;
-		script.src = options.url + ((/\?/).test(options.url) ? '&' : '?') + 'cb=GoPushCli.' + callback;
-		head.insertBefore(script, head.firstChild);
-	};
+    var getScript = function (options) {
+        // JSONP
+        var callback = 'callback_' + Math.floor(new Date().getTime() * Math.random()).toString(36);
+        var head = document.getElementsByTagName("head")[0];
+        var script = document.createElement('script');
+        options = options || {};
+        GoPushCli[callback] = options.success || function () {
+        };
+        script.type = 'text/javascript';
+        script.charset = 'UTF-8';
+        script.onload = script.onreadystatechange = function (_, isAbort) {
+            if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
+                script.onload = script.onreadystatechange = null;
+                head.removeChild(script);
+                script = null;
+            }
+        };
+        //script.src = options.url + ((/\?/).test(options.url) ? '&' : '?') + 'callback=GoPushCli.' + callback;
+        script.src = options.url + ((/\?/).test(options.url) ? '&' : '?') + 'cb=GoPushCli.' + callback;
+        head.insertBefore(script, head.firstChild);
+    };
 
-	var parseJSON = function(data){
-		if(window.JSON && window.JSON.parse){
-			return JSON.parse(data);
-		}
-		return eval('(' + data + ')');
-	};
+    var parseJSON = function (data) {
+        if (window.JSON && window.JSON.parse) {
+            return JSON.parse(data);
+        }
+        return eval('(' + data + ')');
+    };
 
-	var GoPushCli = function(options){
-		// Properties
-		this.host = options.host;
-		this.port = options.port;
-		//构建sessionID {key}_{browser}_{version}_{rn}@{xx}  
-		//                               订阅key_浏览器型号 _浏览器版本号 _随机数@ 
-		//例如:  有@user后缀的12345-Netscape-5-751@user ,
-		//        没有@后缀的自动去掉  12345-Netscape-5-098
-		//1 先判断是否有@user这样的后缀
-		var tmp = options.key;
-		var browser=navigator.appName;
-		var b_version=navigator.appVersion;
-		var version=parseFloat(b_version);
-		var rn = Math.round(Math.random()*999); //三位随机数
- 		 
-		if(tmp.indexOf('@') == -1){
-			tmp = tmp +"-"+browser+"-"+version+"-"+rn;
-		}else{
-			var start = tmp.substring(0,tmp.lastIndexOf('@'));
-			var end = tmp.substring(tmp.lastIndexOf('@'))
-			tmp = start +"-"+browser+"-"+version+"-"+rn+end;
-		}
-		this.type = browser;
-		this.key = tmp;
-		this.heartbeat = options.heartbeat || 60;
-		this.mid = options.mid || 0;
-		this.pmid = options.pmid || 0;
-		this.proto = window.WebSocket ? 1 : 2;
-		// Timers
-		this.heartbeatTimer = null;
-		this.timeoutTimer = null;
-		// Status
-		this.isGetNode = false;
-		this.isHandshake = false;
-		this.isDesotry = false;
-		// Events
-		this.onOpen = options.onOpen || function(){};
-		this.onError = options.onError || function(){};
-		this.onClose = options.onClose || function(){};
-		this.onOnlineMessage = options.onOnlineMessage || function(){};
-		this.onOfflineMessage = options.onOfflineMessage || function(){};
-	};
+    var GoPushCli = function (options) {
+        // Properties
+        this.host = options.host;
+        this.port = options.port;
+        // 构建会话 id： {key}_{browser}_{version}_{rn}@{xx}  
+        
+        var tmp = options.key;
+        var browser = navigator.appName;
+        var b_version = navigator.appVersion;
+        var version = parseFloat(b_version);
+        var rn = Math.round(Math.random() * 999); //三位随机数
 
-	GoPushCli.prototype.start = function(){
-		var that = this;
-		getScript({
-			url: 'http://' + that.host + ':' + that.port + '/1/server/get?k=' + that.key + '&p=' + that.proto,
-			success: function(json){
-				if(json.ret == 0){
-					that.isGetNode = true;
-					if(that.proto == 1){
-						that.initWebSocket(json.data.server.split(':'));
-					}else{
-						// TODO Comet
-						that.onError('浏览器不支持WebSocket');
-					}
-				}else{
-					that.onError(json.msg);
-				}
-			}
-		});
-	};
+        if (tmp.indexOf('@') == -1) {
+            tmp = tmp + "_" + browser + "-" + version + "-" + rn;
+        } else {
+            var start = tmp.substring(0, tmp.lastIndexOf('@'));
+            var end = tmp.substring(tmp.lastIndexOf('@'))
+            tmp = start + "_" + browser + "-" + version + "-" + rn + end;
+        }
+        this.type = browser;
+        this.key = tmp;
+        this.heartbeat = options.heartbeat || 60;
+        this.mid = options.mid || 0;
+        this.pmid = options.pmid || 0;
+        this.proto = window.WebSocket ? 1 : 2;
+        // Timers
+        this.heartbeatTimer = null;
+        this.timeoutTimer = null;
+        // Status
+        this.isGetNode = false;
+        this.isHandshake = false;
+        this.isDesotry = false;
+        // Events
+        this.onOpen = options.onOpen || function () {
+        };
+        this.onError = options.onError || function () {
+        };
+        this.onClose = options.onClose || function () {
+        };
+        this.onOnlineMessage = options.onOnlineMessage || function () {
+        };
+        this.onOfflineMessage = options.onOfflineMessage || function () {
+        };
+    };
 
-	GoPushCli.prototype.initWebSocket = function(node){
-		var that = this;
-		that.ws = new ReconnectingWebSocket('ws://' + node[0] + ':' + parseInt(node[1]) + '/sub?key=' + that.key + '&heartbeat=' + that.heartbeat);
-		//that.ws = new ReconnectingWebSocket('ws://' + node[0] + ':81/sub?key=' + that.key + '&heartbeat=' + that.heartbeat);
-		that.ws.onopen = function(){
-			var key = that.key;
-			var heartbeatStr = that.heartbeat + '';
-			that.getOfflineMessage();
-			that.runHeartbeatTask();
-			that.onOpen();
-		};
-		that.ws.onmessage = function(e){
-			var data = e.data;
-			if(data[0] == '+'){
-				clearTimeout(that.timerOutTimer);
-				// console.log('Debug: 响应心跳');
-			}else if(data[0] == '-'){
-				that.onError('握手协议错误' + data);
-			}else{
-				var message;
-				try{
-					message = parseJSON(data);
-				}catch(e){
-					that.onError('解析返回JSON失败');
-					return;
-				}
-				if(message.gid == 0){
-					if(that.mid < message.mid){
-						that.mid = message.mid;
-					}else{
-						return;
-					}
-				}else{
-					if(that.pmid < message.mid){
-						that.pmid = message.mid;
-					}else{
-						return;
-					}
-				}
-				that.onOnlineMessage(message);
-			}
-		};
-		that.ws.onclose = function(e){
-			that.onClose();
-			that.isDesotry = true;
-			clearInterval(that.heartbeatTimer);
-			clearTimeout(that.timerOutTimer);
-		};
-	};
+    GoPushCli.prototype.start = function () {
+        var that = this;
+        getScript({
+            url: 'http://' + that.host + ':' + that.port + '/1/server/get?k=' + that.key + '&p=' + that.proto,
+            success: function (json) {
+                if (json.ret == 0) {
+                    that.isGetNode = true;
+                    if (that.proto == 1) {
+                        that.initWebSocket(json.data.server.split(':'));
+                    } else {
+                        // TODO Comet
+                        that.onError('浏览器不支持WebSocket');
+                    }
+                } else {
+                    that.onError(json.msg);
+                }
+            }
+        });
+    };
 
-	GoPushCli.prototype.runHeartbeatTask = function(){
-		var that = this;
-		that.heartbeatTimer = setInterval(function(){
-			that.send('h');
-			that.timerOutTimer = setTimeout(function(){
-				that.destory();
-				that.onError('心跳超时');
-			}, (that.heartbeat + 15) * 1000);
-			// console.log('Debug: 请求心跳');
-		}, that.heartbeat * 1000);
-	};
+    GoPushCli.prototype.initWebSocket = function (node) {
+        var that = this;
+        that.ws = new ReconnectingWebSocket('ws://' + node[0] + ':' + parseInt(node[1]) + '/sub?key=' + that.key + '&heartbeat=' + that.heartbeat);
+        //that.ws = new ReconnectingWebSocket('ws://' + node[0] + ':81/sub?key=' + that.key + '&heartbeat=' + that.heartbeat);
+        that.ws.onopen = function () {
+            var key = that.key;
+            var heartbeatStr = that.heartbeat + '';
+            that.getOfflineMessage();
+            that.runHeartbeatTask();
+            that.onOpen();
+        };
+        that.ws.onmessage = function (e) {
+            var data = e.data;
+            if (data[0] == '+') {
+                clearTimeout(that.timerOutTimer);
+                // console.log('Debug: 响应心跳');
+            } else if (data[0] == '-') {
+                that.onError('握手协议错误' + data);
+            } else {
+                var message;
+                try {
+                    message = parseJSON(data);
+                } catch (e) {
+                    that.onError('解析返回JSON失败');
+                    return;
+                }
+                if (message.gid == 0) {
+                    if (that.mid < message.mid) {
+                        that.mid = message.mid;
+                    } else {
+                        return;
+                    }
+                } else {
+                    if (that.pmid < message.mid) {
+                        that.pmid = message.mid;
+                    } else {
+                        return;
+                    }
+                }
+                that.onOnlineMessage(message);
+            }
+        };
+        that.ws.onclose = function (e) {
+            that.onClose();
+            that.isDesotry = true;
+            clearInterval(that.heartbeatTimer);
+            clearTimeout(that.timerOutTimer);
+        };
+    };
 
-	GoPushCli.prototype.send = function(data){
-		if(this.proto == 1){
-			this.ws.send(data);
-		}else{
-			// Comet TODO
-		}
-	};
+    GoPushCli.prototype.runHeartbeatTask = function () {
+        var that = this;
+        that.heartbeatTimer = setInterval(function () {
+            that.send('h');
+            that.timerOutTimer = setTimeout(function () {
+                that.destory();
+                that.onError('心跳超时');
+            }, (that.heartbeat + 15) * 1000);
+            // console.log('Debug: 请求心跳');
+        }, that.heartbeat * 1000);
+    };
 
-	GoPushCli.prototype.getOfflineMessage = function(){
-		var that = this;
-		getScript({
-			url: 'http://' + that.host + ':' + that.port + '/1/msg/get?k=' + that.key +'&t='+ that.type + '&m=' + that.mid + '&p=' + that.pmid,
-			success: function(json){
-				if(json.ret == 0){
-					var message;
-					var data = json.data;
-					if(data && data.pmsgs){
-						for(var i = 0, l = data.pmsgs.length; i < l; ++i){
-							message = parseJSON(data.pmsgs[i]);
-							if(that.pmid < message.mid){
-								that.onOfflineMessage(message);
-								that.pmid = message.mid;
-							}
-						}
-					}
-					if(data && data.msgs){
-						for(var i = 0, l = data.msgs.length; i < l; ++i){
-							message = parseJSON(data.msgs[i]);
-							if(that.mid < message.mid){
-								that.onOfflineMessage(message);
-								that.mid = message.mid;
-							}
-						}
-					}
-				}else{
-					that.onError(json.msg);
-				}
-			}
-		});
-	};
+    GoPushCli.prototype.send = function (data) {
+        if (this.proto == 1) {
+            this.ws.send(data);
+        } else {
+            // Comet TODO
+        }
+    };
 
-	GoPushCli.prototype.destory = function(){
-		this.ws.close();
-	};
+    GoPushCli.prototype.getOfflineMessage = function () {
+        var that = this;
+        getScript({
+            url: 'http://' + that.host + ':' + that.port + '/1/msg/get?k=' + that.key + '&t=' + that.type + '&m=' + that.mid + '&p=' + that.pmid,
+            success: function (json) {
+                if (json.ret == 0) {
+                    var message;
+                    var data = json.data;
+                    if (data && data.pmsgs) {
+                        for (var i = 0, l = data.pmsgs.length; i < l; ++i) {
+                            message = parseJSON(data.pmsgs[i]);
+                            if (that.pmid < message.mid) {
+                                that.onOfflineMessage(message);
+                                that.pmid = message.mid;
+                            }
+                        }
+                    }
+                    if (data && data.msgs) {
+                        for (var i = 0, l = data.msgs.length; i < l; ++i) {
+                            message = parseJSON(data.msgs[i]);
+                            if (that.mid < message.mid) {
+                                that.onOfflineMessage(message);
+                                that.mid = message.mid;
+                            }
+                        }
+                    }
+                } else {
+                    that.onError(json.msg);
+                }
+            }
+        });
+    };
 
-	window.GoPushCli = GoPushCli;
+    GoPushCli.prototype.destory = function () {
+        this.ws.close();
+    };
+
+    window.GoPushCli = GoPushCli;
 })();
 
 // MIT License:
@@ -257,7 +260,7 @@
 (function (global, factory) {
     if (typeof define === 'function' && define.amd) {
         define([], factory);
-    } else if (typeof module !== 'undefined' && module.exports){
+    } else if (typeof module !== 'undefined' && module.exports) {
         module.exports = factory();
     } else {
         global.ReconnectingWebSocket = factory();
@@ -278,39 +281,39 @@
         var ws;
         var forcedClose = false;
         var timedOut = false;
-        
+
         this.url = url;
         this.protocols = protocols;
         this.readyState = WebSocket.CONNECTING;
         this.URL = url; // Public API
 
-        this.onopen = function(event) {
+        this.onopen = function (event) {
         };
 
-        this.onclose = function(event) {
+        this.onclose = function (event) {
         };
 
-        this.onconnecting = function(event) {
+        this.onconnecting = function (event) {
         };
 
-        this.onmessage = function(event) {
+        this.onmessage = function (event) {
         };
 
-        this.onerror = function(event) {
+        this.onerror = function (event) {
         };
 
         function connect(reconnectAttempt) {
             ws = new WebSocket(url, protocols);
-            
-            if(!reconnectAttempt)
+
+            if (!reconnectAttempt)
                 self.onconnecting();
-                
+
             if (self.debug || ReconnectingWebSocket.debugAll) {
                 console.debug('ReconnectingWebSocket', 'attempt-connect', url);
             }
-            
+
             var localWs = ws;
-            var timeout = setTimeout(function() {
+            var timeout = setTimeout(function () {
                 if (self.debug || ReconnectingWebSocket.debugAll) {
                     console.debug('ReconnectingWebSocket', 'connection-timeout', url);
                 }
@@ -318,8 +321,8 @@
                 localWs.close();
                 timedOut = false;
             }, self.timeoutInterval);
-            
-            ws.onopen = function(event) {
+
+            ws.onopen = function (event) {
                 clearTimeout(timeout);
                 if (self.debug || ReconnectingWebSocket.debugAll) {
                     console.debug('ReconnectingWebSocket', 'onopen', url);
@@ -329,8 +332,8 @@
                 self.reconnectAttempts = 0;
                 self.onopen(event);
             };
-            
-            ws.onclose = function(event) {
+
+            ws.onclose = function (event) {
                 clearTimeout(timeout);
                 ws = null;
                 if (forcedClose) {
@@ -345,19 +348,19 @@
                         }
                         self.onclose(event);
                     }
-                    setTimeout(function() {
+                    setTimeout(function () {
                         self.reconnectAttempts++;
                         connect(true);
                     }, self.reconnectInterval * Math.pow(self.reconnectDecay, self.reconnectAttempts));
                 }
             };
-            ws.onmessage = function(event) {
+            ws.onmessage = function (event) {
                 if (self.debug || ReconnectingWebSocket.debugAll) {
                     console.debug('ReconnectingWebSocket', 'onmessage', url, event.data);
                 }
                 self.onmessage(event);
             };
-            ws.onerror = function(event) {
+            ws.onerror = function (event) {
                 if (self.debug || ReconnectingWebSocket.debugAll) {
                     console.debug('ReconnectingWebSocket', 'onerror', url, event);
                 }
@@ -366,7 +369,7 @@
         }
         connect(false);
 
-        this.send = function(data) {
+        this.send = function (data) {
             if (ws) {
                 if (self.debug || ReconnectingWebSocket.debugAll) {
                     console.debug('ReconnectingWebSocket', 'send', url, data);
@@ -377,7 +380,7 @@
             }
         };
 
-        this.close = function() {
+        this.close = function () {
             forcedClose = true;
             if (ws) {
                 ws.close();
@@ -388,7 +391,7 @@
          * Additional public API method to refresh the connection if still open (close, re-open).
          * For example, if the app suspects bad data / missed heart beats, it can try to refresh.
          */
-        this.refresh = function() {
+        this.refresh = function () {
             if (ws) {
                 ws.close();
             }
